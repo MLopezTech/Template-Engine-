3d2dcaad981668aebf781e3d7a6139e0eabe7efc
Miguel Lopez <46061256+chino6989@users.noreply.github.com>
2020-09-14 20:58:27 -0400

    Add files via upload

---
+++ Template-Engine-Employee-Summary/library/employee.js
@@ -0,0 +1,29 @@
+
+class Employee {
+  
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+    };
+
+    // name
+    getName() {
+      return this.name;
+    };
+  
+    // id
+    getId() {
+      return this.id;
+    };
+    
+    // email
+    getEmail() {
+      return this.email;
+    };
+     getRole() {
+      return "Employee";
+    };
+  };
+  
+  module.exports = Employee;
